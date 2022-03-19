77aea551e72459b19ce003a791195decc0d7ba86
Stefan Hoffmann <stho32github@gmail.com>
2022-03-19 15:18:40 +0000

    Add DatabaseColumn implementation

---
+++ Source/genny/genny.BL/Entities/DatabaseColumn.cs
@@ -0,0 +1,23 @@
+﻿using genny.Interfaces.Entities;
+
+namespace genny.BL.Entities;
+
+public class DatabaseColumn : IDatabaseColumn
+{
+    public string Name { get; }
+    public string DataType { get; }
+    public int MaxLength { get; }
+    public int Precision { get; }
+    public int Scale { get; }
+    public bool IsPrimaryKey { get; }
+
+    public DatabaseColumn(string name, string dataType, int maxLength, int precision, int scale, bool isPrimaryKey)
+    {
+        Name = name;
+        DataType = dataType;
+        MaxLength = maxLength;
+        Precision = precision;
+        Scale = scale;
+        IsPrimaryKey = isPrimaryKey;
+    }
+}
